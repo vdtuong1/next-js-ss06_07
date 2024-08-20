cdc869d9369cc184455275c1dbb882035a540bea
vdtuong1 <147390887+vdtuong1@users.noreply.github.com>
2024-08-20 11:46:43 +0700

    Add files via upload

---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '@/components/Button'
+export default function DashBoard() {
+  return (
+    <div>
+      Đây là trang dashboard
+      <Button></Button>
+    </div>
+  )
+}
